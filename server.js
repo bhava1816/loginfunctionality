87b5a72345d1297a1ba21e83bfb637f24c390a35
Bhava Narayana <bhavanarayanachukka@gmail.com>
2025-11-07 15:49:17 +0530

    commited succesfully123

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-
-let mongoose=require("mongoose");
-let express=require("express")
-let cors=require("cors")
+let mongoose = require("mongoose");
+let express = require("express");
+let cors = require("cors");
 require("dotenv").config();
-let databaseconnection=require("./Router/Database")
-let mainfunction=require("./Router/Routes")
-let app=express();
-app.use(cors())
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+let databaseconnection = require("./Router/Database");
+let mainfunction = require("./Router/Routes");
+
 const path = require("path");
 
+let app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 1. Connect to DB
+databaseconnection();
+
+// 2. API routes FIRST
+app.use("/", mainfunction);
+
+// 3. Serve React build folder
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-
-app.get("*", (req, res) => {
+// 4. Wildcard route for React (Express 5 compatible)
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-databaseconnection()
-app.use("/",mainfunction)
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running in the${process.env.PORT}port`)
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-   
