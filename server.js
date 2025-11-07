51df3c6a4548e4cdec402f2569ff45692ac3ddf0
Bhava Narayana <bhavanarayanachukka@gmail.com>
2025-11-07 14:57:05 +0530

    commited succesfully

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}))
 databaseconnection()
 app.use("/",mainfunction)
 app.listen(process.env.PORT,()=>{
-    console.log("server is running in the 2222 port")
+    console.log(`server is running in the${process.env.PORT}port`)
 });
 
 
